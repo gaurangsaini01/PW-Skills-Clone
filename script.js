7e6b99e8b5c1f5656ce191663e25200375eeca24
gaurangsaini01 <gaurangsaini01@gmail.com>
2023-07-15 12:17:24 +0530

    small changes in hamburger menu

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const navLinks=document.querySelector(".nav-links")
 function onToggle(e){
     e.name=e.name==='menu'?'close':'menu';
-    navLinks.classList.toggle('top-[100%]')
+    navLinks.classList.toggle('left-[0%]')
 }
